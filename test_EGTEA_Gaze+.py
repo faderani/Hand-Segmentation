152e06cebfed18c68fbd96417c35238b133c70d8
soroush <shahi.varnus@gmail.com>
2021-08-23 10:27:31 -0500

    adding batch support

--- test_EGTEA_Gaze+.py
+++ test_EGTEA_Gaze+.py
@@ -21,22 +21,35 @@ def predict_img(net, full_img, gpu=False):
 
     right = np.transpose(right, axes=[2, 0, 1])
     left = np.transpose(left, axes=[2, 0, 1])
+    
+    right = np.array([right]*20)
+    left = np.array([left]*20)
+    with torch.no_grad():
 
-    X_l = torch.FloatTensor(left).unsqueeze(0)
-    X_r = torch.FloatTensor(right).unsqueeze(0)
+        X_l = torch.FloatTensor(left)
+        X_r = torch.FloatTensor(right)
+        
+        
+        if gpu:
+            X_l = Variable(X_l, volatile=True).cuda()
+            X_r = Variable(X_r, volatile=True).cuda()
+        else:
+            X_l = Variable(X_l, volatile=True)
+            X_r = Variable(X_r, volatile=True)
 
-    if gpu:
-        X_l = Variable(X_l, volatile=True).cuda()
-        X_r = Variable(X_r, volatile=True).cuda()
-    else:
-        X_l = Variable(X_l, volatile=True)
-        X_r = Variable(X_r, volatile=True)
+        y_l = F.sigmoid(net(X_l))
+        y_r = F.sigmoid(net(X_r))
+        
+    y_l = F.upsample_bilinear(y_l, scale_factor=2).data.cpu().numpy()
+    y_r = F.upsample_bilinear(y_r, scale_factor=2).data.cpu().numpy()
+        
+#     y_l = F.upsample_bilinear(y_l, scale_factor=2).data[0][0].cpu().numpy()
+#     y_r = F.upsample_bilinear(y_r, scale_factor=2).data[0][0].cpu().numpy()
 
-    y_l = F.sigmoid(net(X_l))
-    y_r = F.sigmoid(net(X_r))
-    y_l = F.upsample_bilinear(y_l, scale_factor=2).data[0][0].cpu().numpy()
-    y_r = F.upsample_bilinear(y_r, scale_factor=2).data[0][0].cpu().numpy()
-
+    print(y_l.shape)
+    
+    fghf()
+    
     y = merge_masks(y_l, y_r, full_img.size[0])
     yy = dense_crf(np.array(full_img).astype(np.uint8), y)
 
@@ -50,8 +63,10 @@ if __name__ == "__main__":
                         help="Specify the file in which is stored the model"
                              " (default : 'MODEL.pth')")
     parser.add_argument('--dataset-path', help='path to root of image dataset', required=True)
-    parser.add_argument('--output', '-o', metavar='INPUT', nargs='+',
+    parser.add_argument('--output', '-o',
                         help='directory to output the predictions', required=True)
+    parser.add_argument('--batch-size', '-b',
+                        help='batch size for prediction', default=32)
     parser.add_argument('--cpu', '-c', action='store_true',
                         help="Do not use the cuda version of the net",
                         default=False)
@@ -76,16 +91,29 @@ if __name__ == "__main__":
     print("Loading model ...")
     net.load_state_dict(torch.load(args.model))
     print("Model loaded !")
+    
+    
+    batches = [all_jpg_files[i:i + args.batch_size] for i in range(0, len(all_jpg_files), args.batch_size)]
 
-    for i, fn in enumerate(all_jpg_files):
+    for i, fn in enumerate(batches):
+        
+        if i%1000==0:
+            print(i/len(all_jpg_files) * 100)
+            
+        output_paths = [os.path.join(args.output, *x.split("/")[-3:]) for x in fn]
+        
+#         if os.path.exists(output_paths):
+#             continue
+        
         #print("\nPredicting image {} ...".format(fn))
         img = Image.open(fn)
         out = predict_img(net, img, not args.cpu)
         
         
-        output_path = os.path.join(args.output, *fn.split("/")[-2:])
-        
-        os.makedirs(os.path.join(*output_path.split("/")[0:-1]))
+        #print(fn.split("/")[-2:])
+        for path in output_paths:
+            if os.path.exists(os.path.join(*path.split("/")[0:-1])) == False:
+                os.makedirs(os.path.join(*path.split("/")[0:-1]))
         
        
         result = Image.fromarray((out * 255).astype(numpy.uint8))
